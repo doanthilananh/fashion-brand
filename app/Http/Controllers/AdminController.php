e7ac8ed5fe58902a7339d950351705caa1adcc73
Kim Phượng <nguyenthikimphuong2209@gmail.com>
2022-08-18 16:56:31 +0700

    done api login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,6 +16,7 @@ class AdminController extends Controller
     public function dashboard(){
         return view ('backend.home');
     }
+    #phuong
     public function login(Request $request){
         $admin_email = $request->admin_email;
         $admin_password = md5($request->admin_password);
